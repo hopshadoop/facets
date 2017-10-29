87d5b514c27e647e03a17de9582cce6d3ba30895
Jim Dowling <jdowling@sics.se>
2017-10-29 14:59:32 +0100

    just python code for pypi now

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+echo "cleaning dist/*"
+rm -rf dist/*
+
+echo "Generating the binary package...."
+python setup.py bdist_wheel --universal
+python setup.py sdist
+
+echo "Uploading the package to PyPi"
+twine upload dist/*
+
