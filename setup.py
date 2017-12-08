a2b52fe55b627306f737f146f8d92771014cd7f3
Jim Dowling <jdowling@sics.se>
2017-12-08 11:29:30 +0100

    remove tensorflow dependency

--- setup.py
+++ setup.py
@@ -73,6 +73,6 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['jupyter', 'tensorflow', 'numpy', 'pandas', 'protobuf'],
+    install_requires=['jupyter', 'numpy', 'pandas', 'protobuf'],
 )
 
